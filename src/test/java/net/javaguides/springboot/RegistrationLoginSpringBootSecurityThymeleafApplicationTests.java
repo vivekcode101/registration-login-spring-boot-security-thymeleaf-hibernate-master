1036cb64026c3f57197a81bb41bb9c2c53367549
vivekcode101 <vivekcode101@gmail.com>
2023-03-31 14:39:52 +0530

    Changes in main application.java

--- src/test/java/net/javaguides/springboot/RegistrationLoginSpringBootSecurityThymeleafApplicationTests.java
+++ src/test/java/net/javaguides/springboot/RegistrationLoginSpringBootSecurityThymeleafApplicationTests.java
@@ -1,13 +1,15 @@
 package net.javaguides.springboot;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootTest
-class RegistrationLoginSpringBootSecurityThymeleafApplicationTests {
+@SpringBootApplication
+public class RegistrationLoginSpringBootSecurityThymeleafApplicationTests {
 
-	@Test
-	void contextLoads() {
+	public static void main(String[] args) {
+		SpringApplication.run(RegistrationLoginSpringBootSecurityThymeleafApplication.class,args);
 	}
 
 }
+
+
